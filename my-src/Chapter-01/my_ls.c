b72810114641efa57bb1da081104c86baff79ee5
lelouch <lelouch.279978955@gmail.com>
2018-07-07 20:37:12 +0800

    my ls

--- my-src/Chapter-01/my_ls.c
+++ my-src/Chapter-01/my_ls.c
@@ -9,10 +9,13 @@ int main(int argc, char *argv[]) {
         err_quit("usage: ls directory_name");
     }
 
-    if ((dp == opendir(argv[1])) == NULL) {
+    if ((dp = opendir(argv[1])) == NULL) {
         err_sys("cannot open %s", argv[1]);
     }
-    while ((dirp == readdir(dp)) != NULL) {
+    while ((dirp = readdir(dp)) != NULL) {
         printf("%s\n", dirp->d_name);
     }
+
+    closedir(dp);
+    exit(0);
 }
