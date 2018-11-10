a54aaf01a5d550c3d380e74532cfd31dc4715419
lelouch <lelouch.279978955@gmail.com>
2018-11-10 08:44:39 +0800

    add annotation to 18-10.c

--- my-src/Chapter-18/18-10.c
+++ my-src/Chapter-18/18-10.c
@@ -6,18 +6,26 @@ main(void) {
     struct termios  term;
     long            vdisable;
 
+    // 仅当标准输入是终端设备时才修改终端特殊字符
     if (isatty(STDIN_FILENO) == 0)
         err_quit("standard input is not a terminal device");
 
+    // 获取_POSIX_VDISABLE值
     if ((vdisable = fpathconf(STDIN_FILENO, _PC_VDISABLE)) < 0)
         err_quit("fpathconf error or _POSIX_VDISABLE not in effect");
 
+    // 获取termios结构
     if (tcgetattr(STDIN_FILENO, &term) < 0)
         err_sys("tcgetattr error");
 
+    // 禁用中断
+    // 注意这和忽略中断信号是不同的
+    // 这里只是禁用使终端驱动程序产生SIGINT信号的特殊字符(control + C)
+    // 但是我们仍然可以使用kill函数将信号发送至进程
     term.c_cc[VINTR] = vdisable;
     term.c_cc[VEOF]  = 2;
 
+    // 设置属性
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &term) < 0)
         err_sys("tcsetattr error");
 
